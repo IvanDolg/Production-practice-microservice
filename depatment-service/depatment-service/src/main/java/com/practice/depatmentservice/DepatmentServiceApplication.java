e4f96e24d85931b6fadcf078ca083b8ad51643e8
Ivan Dolgolaptev <ivandolgolaptev@gmail.com>
2024-06-14 12:02:37 +0300

    init: Initial commit

---
+++ depatment-service/depatment-service/src/main/java/com/practice/depatmentservice/DepatmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.practice.depatmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepatmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepatmentServiceApplication.class, args);
+	}
+
+}
---
+++ depatment-service/depatment-service/src/test/java/com/practice/depatmentservice/DepatmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.depatmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepatmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
