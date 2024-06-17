567a931328fbd585677afe635c7a415798e3f22a
Ivan Dauhalaptseu <ivan.dauhalaptseu@gmail.com>
2024-06-17 14:02:35 +0300

    init: start working with service-registry

--- depatment-service/depatment-service/src/main/java/com/practice/depatmentservice/service/DepartmentService.java
+++
@@ -1,8 +0,0 @@
-package com.practice.depatmentservice.service;
-
-import com.practice.depatmentservice.dto.DepartmentDto;
-
-public interface  DepartmentService {
-    DepartmentDto saveDepartment(DepartmentDto departmentDto);
-    DepartmentDto getDepartmentByCode(String departmentCode);
-}
