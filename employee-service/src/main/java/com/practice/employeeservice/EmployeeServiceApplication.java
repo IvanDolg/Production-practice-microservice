a34929012a2e71a786a89b809bd90d57d887442f
Ivan Dauhalaptseu <ivan.dauhalaptseu@gmail.com>
2024-06-17 15:54:00 +0300

    feat: service registry and Discovery using spring cloud Netflix Eureka

--- employee-service/src/main/java/com/practice/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/practice/employeeservice/EmployeeServiceApplication.java
@@ -3,6 +3,7 @@ package com.practice.employeeservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaServer
 public class   EmployeeServiceApplication {
 	@Bean
 	public ModelMapper modelMapper(){
