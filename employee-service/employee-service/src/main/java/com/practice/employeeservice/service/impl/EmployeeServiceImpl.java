14f743b4eff24c62e37026377ce6b90ac2c13df9
Ivan Dauhalaptseu <ivan.dauhalaptseu@gmail.com>
2024-06-17 12:24:12 +0300

    feat: Microservice communication using WebClient

--- employee-service/employee-service/src/main/java/com/practice/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/employee-service/src/main/java/com/practice/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -13,6 +13,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Optional;
 
@@ -21,7 +22,9 @@ import java.util.Optional;
 public class EmployeeServiceImpl implements EmployeeService {
 
     private  EmployeeRepository employeeRepository;
-    private RestTemplate restTemplate;
+
+    //private RestTemplate restTemplate;
+    private WebClient webClient;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -44,9 +47,16 @@ public class EmployeeServiceImpl implements EmployeeService {
                 () -> new RecurseNotFoundException("Employee", "id", employeeId)
         );
 
-        ResponseEntity <DepartmentDto> response = restTemplate.getForEntity("http://localhost:8080/api/departments" + employee.getDepartmentCode(), DepartmentDto.class);
+//        ResponseEntity <DepartmentDto> response = restTemplate.getForEntity("http://localhost:8080/api/departments" +
+//                employee.getDepartmentCode(), DepartmentDto.class);
+//
+//        DepartmentDto departmentDto = response.getBody();
 
-        DepartmentDto departmentDto = response.getBody();
+        DepartmentDto departmentDto =webClient.get()
+                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
 
         EmployeeDto employeeDto = AutoEmployeeMapper.MAPPER.mapToEmployeeDto(employee);
 
