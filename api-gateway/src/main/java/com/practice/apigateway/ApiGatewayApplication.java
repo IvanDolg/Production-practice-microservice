3aef8a4305ff914f557d456e2be4e8f5b0fb98c9
Ivan Dauhalaptseu <ivan.dauhalaptseu@gmail.com>
2024-06-19 10:53:51 +0300

    fix: fix api Gateway

--- api-gateway/src/main/java/com/practice/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/practice/apigateway/ApiGatewayApplication.java
@@ -1,11 +1,11 @@
 package com.practice.apigateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableEurekaServer
+//@EnableEurekaServer
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
--- employee-service/src/main/java/com/practice/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/practice/employeeservice/EmployeeServiceApplication.java
@@ -3,13 +3,13 @@ package com.practice.employeeservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableEurekaServer
+//@EnableEurekaServer
 public class   EmployeeServiceApplication {
 	@Bean
 	public ModelMapper modelMapper(){
