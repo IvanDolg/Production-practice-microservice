2fbd8943944cb842c0511e0758702b11d215f75c
Ivan Dauhalaptseu <ivan.dauhalaptseu@gmail.com>
2024-06-20 12:43:24 +0300

    init: add new service - Organization-service

--- employee-service/src/main/java/com/practice/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/practice/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -10,8 +10,12 @@ import com.practice.employeeservice.mapper.AutoEmployeeMapper;
 import com.practice.employeeservice.repository.EmployeeRepository;
 import com.practice.employeeservice.service.APIClient;
 import com.practice.employeeservice.service.EmployeeService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
 import lombok.val;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -19,42 +23,75 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Optional;
 
+
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
 
+    private static  final  Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
     private  EmployeeRepository employeeRepository;
+
+    //private RestTemplate restTemplate;
+    private WebClient webClient;
     private APIClient apiClient;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
-
         Optional<Employee> optionalEmployee = employeeRepository.findByEmail(employeeDto.getEmail());
         if (optionalEmployee.isPresent()) {
             throw new EmailAlreadyExistsException("Email already exists");
         }
-
         Employee employee = AutoEmployeeMapper.MAPPER.mapToEmployee(employeeDto);
         Employee savedEmployee = employeeRepository.save(employee);
-
         return AutoEmployeeMapper.MAPPER.mapToEmployeeDto(savedEmployee);
     }
 
+    //@CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
+    @Retry(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
     @Override
     public APIResponseDto getEmployeeById(Long employeeId) {
 
+        LOGGER.info("instance getEmployeeById() method");
         Employee employee = employeeRepository.findById(employeeId).orElseThrow(
                 () -> new RecurseNotFoundException("Employee", "id", employeeId)
         );
 
-        DepartmentDto departmentDto = apiClient.getDepartmentByCode(employee.getDepartmentCode());
+//        ResponseEntity <DepartmentDto> response = restTemplate.getForEntity("http://localhost:8080/api/departments" +
+//                employee.getDepartmentCode(), DepartmentDto.class);
+//
+//        DepartmentDto departmentDto = response.getBody();
+
+        DepartmentDto departmentDto =webClient.get()
+                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
+
+        //DepartmentDto departmentDto = apiClient.getDepartmentByCode(employee.getDepartmentCode());
 
         EmployeeDto employeeDto = AutoEmployeeMapper.MAPPER.mapToEmployeeDto(employee);
-
         APIResponseDto apiResponseDto = new APIResponseDto();
         apiResponseDto.setEmployee(employeeDto);
         apiResponseDto.setDepartment(departmentDto);
+        return apiResponseDto;
+    }
 
+    public APIResponseDto getDefaultDepartment(Long employeeId, Exception exception) {
+
+        LOGGER.info("instance getDefaultDepartment() method");
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                () -> new RecurseNotFoundException("Employee", "id", employeeId)
+        );
+
+        DepartmentDto departmentDto = new DepartmentDto();
+        departmentDto.setDepartmentName("Default Department");
+        departmentDto.setDepartmentDescription("Default Description");
+        departmentDto.setDepartmentCode("Default Code");
+
+        EmployeeDto employeeDto = AutoEmployeeMapper.MAPPER.mapToEmployeeDto(employee);
+        APIResponseDto apiResponseDto = new APIResponseDto();
+        apiResponseDto.setEmployee(employeeDto);
+        apiResponseDto.setDepartment(departmentDto);
         return apiResponseDto;
     }
 }
